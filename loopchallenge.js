2b4c657a5e47a2fdf65337852d064fc7a6c93807
AndyM714 <112412638+AndyM714@users.noreply.github.com>
2022-09-16 11:25:52 -0700

    Add files via upload

---
+++ loopchallenge.js
@@ -0,0 +1,32 @@
+for (i=1; i<=20; i+=2) {
+    console.log(i)
+}
+// Printing the odds of 1-20
+
+for (i=100; i>=0; i--) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
+// Decreasing the multiples of 3 
+
+for (i=4; i>= -3.5; i-=1.5) {
+    console.log(i);
+}
+// Printing the value of sequences
+
+sum = 0;
+for (i=1; i<=100; i++) {
+    sum += i;
+
+    console.log(sum);
+}
+// Adding the values  of 1-100 to Sum
+
+product = 1;
+for (i=1; i<=12; i++) {
+    product *= i;
+
+    console.log(product);
+}
+// Multiplying values of 1-12 into Product
