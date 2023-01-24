d5314c5554c69bdc5976e2c08e1156a4c4c518bd
AndyM714 <112412638+AndyM714@users.noreply.github.com>
2023-01-23 19:39:50 -0800

    Add files via upload

---
+++ Jokes Api/server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+
+const database = 'jokes_db';
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb://localhost/${database}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Established a connection to the database'))
+    .catch(err => console.log('Something went wrong when connecting to the database ', err));
