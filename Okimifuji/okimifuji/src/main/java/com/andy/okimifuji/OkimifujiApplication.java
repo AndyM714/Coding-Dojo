c33de454c87aa258a0458c7aa4b5c5e19960b0d2
AndyM714 <112412638+AndyM714@users.noreply.github.com>
2023-02-17 14:44:34 -0800

    Add files via upload

---
+++ Okimifuji/okimifuji/src/main/java/com/andy/okimifuji/OkimifujiApplication.java
@@ -0,0 +1,13 @@
+package com.andy.okimifuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkimifujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkimifujiApplication.class, args);
+	}
+
+}
